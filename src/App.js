67b78984127591ea6a8a74f5ccf8c8e572633b4c
Dennis Ponce <denizponce16@gmail.com>
2022-01-29 20:14:13 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { HashRoute, Routes, Route } from "react-router-dom";
+import NameForm from './components/NameForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRoute>
+        <Routes>
+          <Route path="/" element={<NameForm/>}/>
+        </Routes>
+      </HashRoute>
     </div>
   );
 }
